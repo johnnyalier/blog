e104b2421f4c4564c3a50beb95229a621160eb78
David Kenyi <davidkenyi@outlook.com>
2024-07-24 20:30:21 -0400

    Added pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
 
 const App = () =>{
-	const [count, setCount] = useState(0)
 
 	return (
-		<h1 className='text-red-500'>Mern Blog Project</h1>
+		<BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
+        // Add your routes here.
 	)
 }
 

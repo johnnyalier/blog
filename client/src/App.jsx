e60565fe4c3ae96b538cb427795d894c0b4d735c
David Kenyi <davidkenyi@outlook.com>
2024-07-25 00:16:03 -0400

    Complete responsive navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
+import Header from "./components/Header";
 
 const App = () =>{
 
 	return (
 		<BrowserRouter>
+			<Header />
             <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/about" element={<About />} />

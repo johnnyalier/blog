fdbecf01fe011e3a1314f94dc5c56a91bffca0bc
David Kenyi <davidkenyi@outlook.com>
2024-07-24 20:12:24 -0400

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const App = () =>{
+	const [count, setCount] = useState(0)
+
+	return (
+		<h1 className='text-red-500'>Mern Blog Project</h1>
+	)
+}
+
+export default App

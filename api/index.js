b69d20c816be21f2b439198774a7cdf4f2d7ec06
David Kenyi <davidkenyi@outlook.com>
2024-07-25 09:23:43 -0400

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from'mongoose';
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser())
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to sudd-estate database')
+    app.listen(PORT, () => {
+        console.log(`Server listening at port ${PORT}`);
+    })
 })

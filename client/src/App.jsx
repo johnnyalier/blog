718bde78c0ebe43aaf159c886829192735d00323
David Kenyi <davidkenyi@outlook.com>
2024-07-26 15:17:41 -0400

    Added authentication to access dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () =>{
 
@@ -17,7 +18,9 @@ const App = () =>{
                 <Route path="/" exact element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/signin" element={<SignIn />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/signup" element={<SignUp />} />
             </Routes>

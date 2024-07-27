bf8e3abac3ab2182f0344665343fdfd9c2e27088
David Kenyi <davidkenyi@outlook.com>
2024-07-27 01:04:10 -0400

    Complete Profile functionality

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { Sidebar } from 'flowbite-react'
+import {
+    HiUser,
+    HiArrowSmRight,
+    HiDocumentText,
+    HiOutlineUserGroup,
+    HiAnnotation,
+    HiChartPie,
+} from 'react-icons/hi';
+
+const DashSidebar = () => {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+
+    useEffect(() => {
+        const tabParam = new URLSearchParams(location.search).get('tab')
+        console.log(tabParam);
+        if (tabParam) setTab(tabParam)
+    }, [location])
+
+
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to='/dashboard?tab=profile'>
+                        <Sidebar.Item 
+                            active={tab === 'profile'} 
+                            icon={HiUser} 
+                            label="User" 
+                            labelColor='dark'
+                        >
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                        Sign out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    )
+}
+
+export default DashSidebar
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashProfile from '../components/DashProfile'
+import DashSidebar from '../components/DashSidebar'
 
 const Dashboard = () => {
+    const location = useLocation()
+    const [tab, setTab] = useState('')
+
+    useEffect(() => {
+        const tabParam = new URLSearchParams(location.search).get('tab')
+        console.log(tabParam);
+        if (tabParam) setTab(tabParam)
+    }, [location.search])
+
     return (
-        <div>Dashboard</div>
+        <div className='min-h-screen flex flex-col md:flex-row'>
+            <div className='md:w-56'>
+                <DashSidebar />
+            </div>
+            {tab === 'profile' && <DashProfile />}
+        </div>
     )
 }
 
